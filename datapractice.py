ca3a8d1beb2a1eacf44037f3f998e856db650e77
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2023-02-01 15:34:25 +0800

    第二節

---
+++ datapractice.py
@@ -0,0 +1,34 @@
+#數字
+19
+
+#浮點數
+3.53
+
+#字串
+"呱呱呱"
+
+#布林值
+True
+False
+
+#list
+a1=[2,4,6]
+a2=["good","so so ","not bad"]
+
+#Tuple
+b1=(2,4,6)
+b2=("good","so so ","not bad")
+
+#set
+c1={2,4,6}
+c2={"good","so so ","not bad"}
+
+#Dictionaray
+d={"1+1":"2","2+2":"4"}
+
+#變數
+變數="變數"
+#打印-print(資料)
+print(變數)
+#test
+print(a1,a2,b1,b2,c1,c2,d)
