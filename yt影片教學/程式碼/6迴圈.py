e61e18fce5ffbdb163b0a323c05b3db12d4ceddd
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2023-02-22 21:32:13 +0800

    loop 學習
    2/22

---
+++ yt影片教學/程式碼/6迴圈.py
@@ -0,0 +1,36 @@
+#while loop
+n=1
+while n<=3:
+    print(n)
+    n+=1
+#等差級數加總
+m=1
+sum=0 #儲存累加的變數
+while m<=10:
+    sum+=m
+    print(sum)
+    m+=1
+
+#for loop
+
+#列表
+for i in [3,5,1]:
+    print(i)
+    
+#字串
+for j in "Hello":
+    print(j)
+    
+#range()
+for k in range(5):
+    print(k)
+    
+#限制範圍
+for l in range(3,7):
+    print(l)
+    
+#等差級數加總
+SUM=0
+for o in range(1,11):
+    SUM+=o
+    print(SUM)
